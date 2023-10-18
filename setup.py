e5c791ce1fcccc79fa93ceed44605c9dc02b6274
Alessandro Conti <alessandro.conti.97@gmail.com>
2023-10-18 12:09:05 +0200

    Bump pytype to 2023.10.17

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 
 setup(
     name='pre_commit_pytype',
-    version='2023.10.5',
-    install_requires=['pytype==2023.10.5'],
+    version='2023.10.17',
+    install_requires=['pytype==2023.10.17'],
 )
