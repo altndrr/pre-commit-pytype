95b19d1dac87e59f69a7eb6a3643854826e4e07b
Alessandro Conti <alessandro.conti.97@gmail.com>
2023-11-04 16:13:38 +0100

    Bump pre-commit to 2023.10.31

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 
 setup(
     name='pre_commit_pytype',
-    version='2023.10.24',
-    install_requires=['pytype==2023.10.24'],
+    version='2023.10.31',
+    install_requires=['pytype==2023.10.31'],
 )
