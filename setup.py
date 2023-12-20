54f9a0100d1326e4fda7a19ae6f3de66aa3e2108
Alessandro Conti <alessandro.conti.97@gmail.com>
2023-12-20 14:51:15 +0100

    Bump pytype to 2023.12.18

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 
 setup(
     name='pre_commit_pytype',
-    version='2023.10.31',
-    install_requires=['pytype==2023.10.31'],
+    version='2023.12.18',
+    install_requires=['pytype==2023.12.18'],
 )
